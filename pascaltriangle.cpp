7286ecd7e3b90b6345bf11424be465bc2abb102d
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-08-24 20:13:24 -0600

    Update pascaltriangle.cpp

--- pascaltriangle.cpp
+++ pascaltriangle.cpp
@@ -5,11 +5,9 @@ using namespace std;
 long fact(int x){
     int i;
     long f=1;
-
     for(i=1;i<=x;++i){
         f=f*i;
     }
-
     return f;
 }
  
@@ -24,7 +22,7 @@ int main(){
         for(k=0;k<=i;++k){
             cout<<fact(i)/(fact(i-k)*fact(k))<<" ";
         }
-        cout<<"\n";//print new line after each row
+        cout<<"\n";
     }
     return 0;
 }
