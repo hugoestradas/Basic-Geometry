ae307392400c2ca2885f1bf8837c376020e6492a
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-09-14 10:12:08 -0600

    Add files via upload

---
+++ Times Table/sketch.js
@@ -0,0 +1,34 @@
+let ratio;
+let factor = 0;
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    ratio = height / 2 - 32;
+}
+
+function getVector(index, total){
+    const angle = map(index % total, 0, total, 0, TWO_PI);
+    const v = p5.Vector.fromAngle(angle + PI);
+    v.mult(ratio);
+    return v;
+}
+
+function draw(){
+    background(0);
+    const total = 200;
+    factor += 0.020;
+
+    translate(width/2, height/2);
+    stroke(255, 150);
+    noFill();
+    ellipse(0, 0, ratio*2);
+
+    strokeWeight(2);
+
+    for(let i=0; i<total; i++){
+        const a = getVector(i, total);
+        const b = getVector(i*factor, total);
+        line(a.x, a.y, b.x, b.y);
+    }
+
+}
